df602c3b5684d197d291be6168c17decefc5bd3b
hitontwo2 <hitontwomc@gmail.com>
2023-04-09 19:12:07 +0530

    lol

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Youtube player via command line
+
+### Note: I made this project cause i have no idea what to do
+
+## **Requirements**
+### Python 3.10(recommended)
+### FFmpeg(including ffplay)
+```sh
+choco install ffmpeg
+``` 
+### YT-Dlp
+```sh
+pip install yt-dlp
+```
+### Youtube-search-python
+```sh
+pip install youtube-search-python
+```
--- ytplay.py
+++ ytplay.py
@@ -2,19 +2,23 @@ import youtubesearchpython
 import yt_dlp as youtube_dl
 import os
 search = input("Search: ")
-result = youtubesearchpython.VideosSearch(search,limit=1)
-url = result.result()["result"][0]["link"]
 
-options = {
+if search is None:
+    print("Please provide a valid argument")
+else:
+    result = youtubesearchpython.VideosSearch(search,limit=1)
+    url = result.result()["result"][0]["link"]
+
+    options = {
     "format":"best",
     "quiet":True,
     "simulate":True,
     "forceurl":True
-}
+    }
 
-result = youtube_dl.YoutubeDL(options).extract_info(url)['url']
-os.system("cls")
-os.system(f'ffplay -autoexit "{result}"')
+    result = youtube_dl.YoutubeDL(options).extract_info(url)['url']
+    os.system("cls")
+    os.system(f'ffplay -autoexit "{result}"')
   
 
 
